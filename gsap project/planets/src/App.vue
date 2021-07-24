4f950717221fb8cba5dcd42a5a106a784abbfe03
KnockOutEZ <ktowhid20@gmail.com>
2021-07-24 22:24:06 +0600

    try

---
+++ gsap project/planets/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Homepage/>
+</template>
+
+<script>
+import Homepage from './components/Homepage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Homepage
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
